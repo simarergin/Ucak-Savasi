e7c315256b9cbe99a014211441794ecef87b10d0
simarergin <simarergin@hotmail.com>
2023-03-16 16:34:13 +0300

    Ucak Savasi

---
+++ Ucak_Savasi/Ucak_Savasi/Ucak_Savasi/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace Ucak_Savasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ducak3 = new System.Windows.Forms.PictureBox();
+            this.ducak1 = new System.Windows.Forms.PictureBox();
+            this.bucak = new System.Windows.Forms.PictureBox();
+            this.mermi = new System.Windows.Forms.PictureBox();
+            this.ducak2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ducak3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ducak1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bucak)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mermi)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ducak2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblsonuc);
+            this.panel1.Location = new System.Drawing.Point(2, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(624, 44);
+            this.panel1.TabIndex = 4;
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.AutoSize = true;
+            this.lblsonuc.Font = new System.Drawing.Font("Century Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsonuc.ForeColor = System.Drawing.Color.White;
+            this.lblsonuc.Location = new System.Drawing.Point(78, 2);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(37, 42);
+            this.lblsonuc.TabIndex = 2;
+            this.lblsonuc.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(10, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 24);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Puan";
+            // 
+            // ducak3
+            // 
+            this.ducak3.Image = global::Ucak_Savasi.Properties.Resources.dusman1;
+            this.ducak3.Location = new System.Drawing.Point(438, 126);
+            this.ducak3.Name = "ducak3";
+            this.ducak3.Size = new System.Drawing.Size(100, 84);
+            this.ducak3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ducak3.TabIndex = 3;
+            this.ducak3.TabStop = false;
+            // 
+            // ducak1
+            // 
+            this.ducak1.Image = global::Ucak_Savasi.Properties.Resources.dusman1;
+            this.ducak1.Location = new System.Drawing.Point(72, 126);
+            this.ducak1.Name = "ducak1";
+            this.ducak1.Size = new System.Drawing.Size(100, 84);
+            this.ducak1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ducak1.TabIndex = 2;
+            this.ducak1.TabStop = false;
+            // 
+            // bucak
+            // 
+            this.bucak.Image = global::Ucak_Savasi.Properties.Resources.bizim;
+            this.bucak.Location = new System.Drawing.Point(258, 498);
+            this.bucak.Name = "bucak";
+            this.bucak.Size = new System.Drawing.Size(107, 83);
+            this.bucak.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.bucak.TabIndex = 0;
+            this.bucak.TabStop = false;
+            // 
+            // mermi
+            // 
+            this.mermi.Image = global::Ucak_Savasi.Properties.Resources.ates;
+            this.mermi.Location = new System.Drawing.Point(294, 422);
+            this.mermi.Name = "mermi";
+            this.mermi.Size = new System.Drawing.Size(23, 37);
+            this.mermi.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.mermi.TabIndex = 0;
+            this.mermi.TabStop = false;
+            // 
+            // ducak2
+            // 
+            this.ducak2.Image = global::Ucak_Savasi.Properties.Resources.dusman1;
+            this.ducak2.Location = new System.Drawing.Point(258, 126);
+            this.ducak2.Name = "ducak2";
+            this.ducak2.Size = new System.Drawing.Size(100, 84);
+            this.ducak2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ducak2.TabIndex = 0;
+            this.ducak2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(624, 667);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.ducak3);
+            this.Controls.Add(this.ducak1);
+            this.Controls.Add(this.bucak);
+            this.Controls.Add(this.mermi);
+            this.Controls.Add(this.ducak2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Uçak Savaşı -- www.bilisimogretmeni.com---";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ducak3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ducak1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bucak)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.mermi)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ducak2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox ducak2;
+        private System.Windows.Forms.PictureBox mermi;
+        private System.Windows.Forms.PictureBox bucak;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox ducak1;
+        private System.Windows.Forms.PictureBox ducak3;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblsonuc;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Ucak_Savasi/Ucak_Savasi/Ucak_Savasi/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ucak_Savasi
+{
+    public partial class Form1 : Form
+    {
+        int solHareket = 0;
+        int ducakHareketHiz = 3;
+        Random rnd = new Random();
+        int mermiHiz = 8;
+        bool ates = false;
+        int Puan = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            ducak1.Top = -500;
+            ducak2.Top = -900;
+            ducak3.Top = -1300;
+            mermi.Top = -100;
+            mermi.Left = -100;
+
+        }
+
+        private void ducak1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                timer1.Start();
+            }
+            if (e.KeyCode == Keys.P)
+            {
+                timer1.Stop();
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                if (bucak.Location.X < 0) { solHareket = 0; } else { solHareket =-5; }
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                if (bucak.Location.X > 512)
+                {
+                    solHareket = 0;
+                }
+                else
+                {
+                    solHareket = 5;
+                }
+            }
+            else if (e.KeyCode == Keys.Space)
+            {
+
+                if (ates == false)
+                {
+                    mermiHiz = 8;
+                    mermi.Left = bucak.Left+40;
+                    mermi.Top = bucak.Top;
+                    ates = true;
+                }
+            }
+
+
+          
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                solHareket = 0;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                solHareket = 0;
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bucak.Left += solHareket;
+            mermi.Top -= mermiHiz;
+            ducak1.Top += ducakHareketHiz;
+            ducak2.Top += ducakHareketHiz;
+            ducak3.Top += ducakHareketHiz;
+            lblsonuc.Text = "" + Puan;
+            if (ducak1.Top >= 660 || ducak2.Top >= 660 || ducak3.Top >= 660)
+            {
+                oyunSonu();
+
+            }
+            if (ates && mermi.Top < 0)
+            {
+                ates = false;
+                mermiHiz = 0;
+                mermi.Top = -100;
+                mermi.Left = -100;
+            }
+            Vurulma();
+        }
+
+        private void oyunSonu()
+        {           
+                timer1.Enabled = false;
+                MessageBox.Show(Puan + " Puan Kazandınız....", "Uçak Oyunu V1.0 www.bilisimogretmeni.com", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                Puan = 0;
+                lblsonuc.Text = "0";
+                ducak1.Top = -500;
+                ducak2.Top = -900;
+                ducak3.Top = -1300;
+                mermi.Top = -100;
+                mermi.Left = -100;
+                timer1.Enabled = true;
+        }
+
+        private void Vurulma()
+        {
+            if (mermi.Bounds.IntersectsWith(ducak1.Bounds))
+            {
+                Puan += 1;
+                ducak1.Top = -500;
+                int ranP = rnd.Next(1, 300);
+                ducak1.Left = ranP;
+                ates = false;
+                mermiHiz = 0;
+                mermi.Top = -100;
+                mermi.Left = -100;
+
+            }
+            else if (mermi.Bounds.IntersectsWith(ducak2.Bounds))
+            {
+                Puan += 1;
+                ducak2.Top = -900;
+                int ranP = rnd.Next(1, 400);
+                ducak2.Left = ranP;
+                ates = false;
+                mermiHiz = 0;
+                mermi.Top = -100;
+                mermi.Left = -100;
+
+            }
+            else if (mermi.Bounds.IntersectsWith(ducak3.Bounds))
+            {
+                Puan += 1;
+                ducak3.Top = -1300;
+                int ranP = rnd.Next(1, 500);
+                ducak3.Left = ranP;
+                ates = false;
+                mermiHiz = 0;
+                mermi.Top = -100;
+                mermi.Left = -100;
+
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
